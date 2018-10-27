47ee2ee17fede0098ea7c586ce4fec84511fc25e
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2018-10-27 16:25:29 +0200

    Prepar bar for C extensions

--- bar/setup.py
+++ bar/setup.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import setuptools
 
+ext_modules = []
+
 config = {
     'name': 'bar',
     'version': '0.1',
@@ -10,6 +12,7 @@ config = {
     'setup_requires': ['pytest-runner'],
     'test_suite': 'tests',
     'tests_require': ['pytest'],
+    'ext_modules': ext_modules,
 }
 
 setuptools.setup(**config)
